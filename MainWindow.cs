638c931e6cc3a12fcfb5f20ab3bc71df1365640e
David <sohodf@gmail.com>
2014-03-23 16:58:59 +0200

    Working

---
+++ MainWindow.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using FTD2XX_NET;
+
+namespace RelaySerialChecker
+{
+    public partial class MainWindow : Form
+    {
+        
+        private FTDI.FT_STATUS _ftStatus;
+        private byte[] _sentBytes = new byte[2];
+        FTDI ftdi = new FTDI();
+        public MainWindow()
+        {
+            InitializeComponent();
+            groupBox1.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = false;
+            //create ftdi object for the relay
+            //check if there are two relays connected
+            if (OpenRelay(ftdi, 1))
+            {
+                MessageBox msg = new MessageBox("Error", "More than one relay connected. Please disconnect all but one relay and try again", this);
+                msg.ShowDialog();
+                ftdi.Close();
+            }
+            else
+            {
+                if (OpenRelay(ftdi, 0))
+                {
+                    textBox1.Text = GetSerial(ftdi);
+                    groupBox1.Visible = true;
+                    ftdi.Close();
+                }
+                else
+                {
+                    MessageBox msg2 = new MessageBox("Error", "Couldn't find/open a relay", this);
+                    msg2.ShowDialog();
+                }
+            }
+        }
+
+        public string GetSerial(FTDI relay)
+        {
+            // Get device info
+            string serialNumber = "";
+            relay.GetSerialNumber(out serialNumber);
+            return serialNumber.ToString();
+            
+        }
+
+
+        //Opens the com port to the relay
+        public Boolean OpenRelay(FTDI ftdi, uint deviceIndex)
+        {
+            _ftStatus = ftdi.OpenByIndex(deviceIndex);
+
+            if (_ftStatus != FTDI.FT_STATUS.FT_OK)
+            {
+                return false;
+            }
+
+            _ftStatus = ftdi.SetBaudRate(921600);
+            if (_ftStatus != FTDI.FT_STATUS.FT_OK)
+            {
+                return false;
+            }
+
+            _ftStatus = ftdi.SetBitMode(255, 4);
+            if (_ftStatus != FTDI.FT_STATUS.FT_OK)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Clipboard.SetText(textBox1.Text);
+        }
+
+        //test the recieved serial
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //open the relay by the new serial
+            FTDI newRelay = new FTDI();
+            string serial = textBox1.Text;
+            newRelay.OpenBySerialNumber(serial);
+
+            byte[] sentBytes = new byte[2];
+            uint receivedBytes = 0;
+            
+            //toggle all on/all off
+
+            sentBytes[0] = (byte)(255);
+            newRelay.Write(sentBytes, 1, ref receivedBytes);
+            System.Threading.Thread.Sleep(1000);
+            sentBytes[0] = (byte)(0);
+            newRelay.Write(sentBytes, 1, ref receivedBytes);
+            newRelay.Close();
+        }
+
+    }
+}
---
+++ MessageBox.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace RelaySerialChecker
+{
+    partial class MessageBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(73, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 100);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MessageBox
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(238, 161);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "MessageBox";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "MessageBox";
+            this.Load += new System.EventHandler(this.MessageBox_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
